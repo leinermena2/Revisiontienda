32f575c9c0c44a274ab89f0c5c0d5b38b2b39ab7
leinermena2 <64869805+leinermena2@users.noreply.github.com>
2020-05-05 21:08:27 -0500

    Add files via upload

---
+++ conexion.php
@@ -0,0 +1,13 @@
+<?php
+   $servidor="localhost";
+   $nombreBd="carrito";
+   $usuario="root";
+   $pass="";
+   $conexion = new mysqli ($servidor,$usuario,$pass,$nombreBd);
+   if($conexion->connect_error ){
+       die("No se pudo conectar");
+   }
+   
+ 
+
+?>
